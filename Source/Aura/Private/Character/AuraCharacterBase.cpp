1c7466b2004dc8114325ff1010895904a571a539
Robert G. Wetherall <robertgwetherall1@gmail.com>
2024-04-27 08:23:41 -0400

    Finished with lesson 6

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,17 @@
+// Copyright Lone Moose Games
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -6,20 +6,15 @@
 #include "GameFramework/Character.h"
 #include "AuraCharacterBase.generated.h"
 
-UCLASS()
+UCLASS(Abstract)
 class AURA_API AAuraCharacterBase : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	AAuraCharacterBase();
-	virtual void Tick(float DeltaTime) override;
-	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 	
 protected:
-	
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 
