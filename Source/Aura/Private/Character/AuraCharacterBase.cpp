d107a8d6ce34ad4eb10bae7f1b28f14c7c33311a
Robert G. Wetherall <robertgwetherall1@gmail.com>
2024-04-28 06:49:23 -0400

    Finished Lesson 7 4/28/2024 6:49am

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -7,7 +7,7 @@ AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
 	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Weapon"));
-	Weapon->SetupAttachment(GetMesh(), GetWeaponHandSocketName());
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
 	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
